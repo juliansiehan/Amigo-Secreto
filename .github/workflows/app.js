579a5836a5751f3f247fa901be6d177b13719d85
juliansiehan <jsiehankiewicz@gmail.com>
2025-03-17 09:39:36 -0300

    add agregarAmigo function

--- .github/workflows/app.js
+++ .github/workflows/app.js
@@ -1,2 +1,16 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let listaAmigos = []
+
+function agregarAmigo () {
+    let campo = document.getElementById('amigo');
+    let nombreAmigo = campo.value 
+    if (nombreAmigo.trim() !== "") {
+        let listaAmigos = document.getElementById('listaAmigos');
+        let nuevoAmigo = document.createElement('li');
+        nuevoAmigo.textContent = nombreAmigo;
+        listaAmigos.appendChild(nuevoAmigo);
+        campo.value = ""
+    } else {
+        alert("Por favor, inserte un nombre")
+    }
+}
